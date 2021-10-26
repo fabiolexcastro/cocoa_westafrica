36e749a4c635c8468b697a6c37e473e610aba29a
Castro <F.CASTRO@cgiar.org>
2021-10-26 16:13:56 -0500

    general

--- 00 general function.R
+++ 00 general function.R
@@ -4,7 +4,9 @@ require(pacman)
 pacman::p_load(raster, rgdal, ggrepel, rgeos, stringr, sf, tidyverse, gtools, 
                ggspatial, terra, ggspatial, gridExtra, ggpubr, ggrepel, ggthemes, hrbrthemes,
                RColorBrewer, Boruta, mlbench, randomForest, caret, fasterize, fs, gridExtra, 
-               colorspace, glue, dismo, usdm, classInt, ENMeval)
+               outliers, Hmisc, cclust,
+               colorspace, glue, dismo, usdm, classInt, ENMeval, ggsn, ggdendro, dendextend, pvclust, ape, 
+               factoextra)
 
 g <- gc(reset = TRUE)
 options(scipen = 999)
