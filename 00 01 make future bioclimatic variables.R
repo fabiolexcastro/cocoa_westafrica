fa0fc3e186d38846f186e2a0f51b9c0d4b0b60be
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-11-25 05:22:02 -0500

    Updating t / 10

--- 00 01 make future bioclimatic variables.R
+++ 00 01 make future bioclimatic variables.R
@@ -24,21 +24,28 @@ get_etp_cm5 <- function(gcm){
   
   # Average temperature
   tav <- (tmx + tmn) / 2
-  tav <- tav / 10
   
   # Change the names
   names(ppt) <- glue('prec_{1:12}')
   names(tmx) <- glue('tmax_{1:12}')
   names(tmn) <- glue('tmin_{1:12}')
+  names(tav) <- glue('tavg_{1:12}')
   
-  # Resampling srad
+  # Resampling srad # Source: https://cgiarcsi.community/data/global-aridity-and-pet-database/ 
   srad <- raster::resample(srad, ppt, method = 'bilinear')
   
-  # To calculte the ETP 
-  etp <- 0.0023 * srad * sqrt(reclassify(tmx - tmn, c(-Inf, 0, 0))) * (tav + 17.8)
-  names(etp) <- glue('etp_{1:12}')
+  # To calculte the ETP  Hargreaves Modified:
+  # Source: https://link.springer.com/content/pdf/10.1023/A:1015508322413.pdf
+  etp <- 0.0013 * 0.408 * srad * (tav + 17) * (tmx - tmn - 0.0123 * ppt) ^ 0.76 
+  names(etp) <- glue('etp_{1:12}') 
   etp <- etp * c(31,29,31,30,31,30,31,31,30,31,30,31)
   etp <- round(etp, 0)
+  
+  # Chech Nans
+  for(i in 1:12){
+    print(i)
+    etp[[i]][which(is.nan(etp[[i]][]))] <- 0
+  }
   
   out <- glue('../data/raster/climate/cmip5/rcp60/2040_2069/{gcm}_etp_{1:12}.tif')
   Map('writeRaster', x = unstack(etp), filename = out, overwrite = TRUE)
@@ -68,7 +75,6 @@ get_bio_cm5 <- function(gcm){
   
   # Average temperature
   tav <- (tmx + tmn) / 2
-  tav <- tav / 10
   
   # To calculate the bioclimatic variables
   mtx <- cbind(as.matrix(etp), as.matrix(ppt), as.matrix(tav))
@@ -99,7 +105,6 @@ get_bio_cm5 <- function(gcm){
   
   cat('Done\n')
 }
-
 get_etp_cm6 <- function(gcm){
   
   cat('Start ', gcm, '\n')
@@ -126,17 +131,22 @@ get_etp_cm6 <- function(gcm){
   srad <- raster::resample(srad, ppt, method = 'bilinear')
   
   # To calculte the ETP 
-  etp <- 0.0023 * srad * sqrt(reclassify(tmx - tmn, c(-Inf, 0, 0))) * (tav + 17.8)
+  etp <- 0.0013 * 0.408 * srad * (tav + 17) * (tmx - tmn - 0.0123 * ppt) ^ 0.76 
   names(etp) <- glue('etp_{1:12}')
   etp <- etp * c(31,29,31,30,31,30,31,31,30,31,30,31)
   etp <- round(etp, 0)
+  
+  # Chech Nans
+  for(i in 1:12){
+    print(i)
+    etp[[i]][which(is.nan(etp[[i]][]))] <- 0
+  }
   
   out <- glue('../data/raster/climate/cmip6/rcp60/2040_2069/etp_{gcm}_ssp370_2041-2060_{1:12}.tif')
   Map('writeRaster', x = unstack(etp), filename = out, overwrite = TRUE)
   cat('Done\n')
   
 }
-
 get_bio_cm6 <- function(gcm){
   
   cat('Start ', gcm, '\n')
@@ -156,7 +166,6 @@ get_bio_cm6 <- function(gcm){
   
   # Average temperature
   tav <- (tmx + tmn) / 2
-  tav <- tav / 10
   
   # To calculate the bioclimatic variables
   mtx <- cbind(as.matrix(etp), as.matrix(ppt), as.matrix(tav))
@@ -175,7 +184,7 @@ get_bio_cm6 <- function(gcm){
   dfc <- ppt - etp
   dft <- cbind(as.matrix(dfc), as.matrix(tmn), as.matrix(tmx))
   bio <- t(apply(dft, 1, cumTemp))
-  dot <- glue('../data/raster/climate/cmip5/rcp60/2040_2069/bio_{gcm}_ssp370_2041-2060_{30:33}.tif')
+  dot <- glue('../data/raster/climate/cmip6/rcp60/2040_2069/bio_{gcm}_ssp370_2041-2060_{30:33}.tif')
   zro <- ppt[[1]]
   
   map(1:ncol(bio), function(i){
@@ -187,7 +196,6 @@ get_bio_cm6 <- function(gcm){
   
   cat('Done\n')
 }
-
 
 # -------------------------------------------------------------------------
 # Srad general 
@@ -201,12 +209,9 @@ names(srad) <- glue('srad_{1:12}')
 # CMIP5 -------------------------------------------------------------------
 # -------------------------------------------------------------------------
 fles <- dir_ls('../data/raster/climate/cmip5/rcp60/2040_2069', regexp = '.tif$')
-prec <- mixedsort(grep('prec', fles, value = TRUE))
-tmax <- mixedsort(grep('tmax', fles, value = TRUE))
-tmin <- mixedsort(grep('tmin', fles, value = TRUE))
 
 # Get each GCM
-gcms <- gsub('_prec_1.tif', '', basename(grep('prec_1.tif$', prec, value = TRUE)))
+gcms <- gsub('_prec_1.tif', '', basename(grep('prec_1.tif$', fles, value = TRUE)))
 
 # To calculate the ETPs ---------------------------------------------------
 map(.x = gcms, .f = get_etp_cm5)
@@ -218,17 +223,16 @@ map(.x = gcms, .f = get_bio_cm5)
 # CMIP6 -------------------------------------------------------------------
 # -------------------------------------------------------------------------
 fles <- dir_ls('../data/raster/climate/cmip6/rcp60/2040_2069', regexp = '.tif$')
-prec <- mixedsort(grep('prec', fles, value = TRUE))
-tmax <- mixedsort(grep('tmax', fles, value = TRUE))
-tmin <- mixedsort(grep('tmin', fles, value = TRUE))
 
 # Get the name of each GCM
-gcms <- gsub('prec_', '', basename(grep('_1.tif$', prec, value = TRUE)))
+gcms <- grep('prec', fles, value = TRUE)
+gcms <- as.character(gcms)
+gcms <- gsub('prec_', '', basename(grep('_1.tif$', gcms, value = TRUE)))
 gcms <- gsub('_ssp370_2041-2060_1.tif', '', gcms)
 
 # To calculate the ETPs ---------------------------------------------------
 map(.x = gcms, .f = get_etp_cm6)
 
 # To calculate the bioclimatic variables ----------------------------------
+fles <- dir_ls('../data/raster/climate/cmip6/rcp60/2040_2069', regexp = '.tif$')
 map(.x = gcms, .f = get_bio_cm6)
-
