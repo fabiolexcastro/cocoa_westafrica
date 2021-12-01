72e9c2d63d1ebe55f40ce060f48db5adf1f2f60e
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-12-01 14:41:16 -0500

    Update combinations (distint)

--- 10 18 01 difference threshold limitations future modal.R
+++ 10 18 01 difference threshold limitations future modal.R
@@ -3,13 +3,15 @@
 source(file = './00 general function.R')
 
 # Add limitations / mixed -------------------------------------------------
-get_zns <- function(prc){
+get_zns <- function(prc.prb, prc.unc){
   
   # prc <- 1
+  # prc.prb <- 10
+  # prc.unc <- 25
   
   # Filtering the threshold
-  thr.prb <- filter(thrs, percentage == prc) %>% pull(2)
-  thr.unc <- filter(qntl, percentage == prc) %>% pull(2)
+  thr.prb <- filter(thrs, percentage == prc.prb) %>% pull(2)
+  thr.unc <- filter(qntl, percentage == prc.unc) %>% pull(2)
   
   # Matrix
   mcl.prb <- matrix(c(0, thr.prb, 0, thr.prb, 1, 1), ncol = 3, nrow = 2, byrow = TRUE)
@@ -17,7 +19,7 @@ get_zns <- function(prc){
   
   # Bianary raster (prob and cluster)
   bcl <- raster::reclassify(x = mdal, rcl = mcl.cls)
-  bpr <- raster::reclassify(x = prob, rcl = c(0, thr, 0, thr, 1, 2))
+  bpr <- raster::reclassify(x = prob, rcl = c(0, thr.prb, 0, thr.prb, 1, 2))
   
   # To generate the limitations raster
   rsl <- mdal
@@ -28,6 +30,7 @@ get_zns <- function(prc){
   # To generate the mixed raster
   rsl[which(uncr[] < thr.unc  & prob[] > thr.prb)] <- 9
   brs <- raster::reclassify(x = rsl, rcl = c(0, 2.5, 0, 2.5, 7.5, 1, 7.5, 8.5, 2, 8.5, 9.5, 3))
+  plot(brs)
   
   # To make the map
   lbl <- data.frame(x = c(0, 1, 2, 3), y = c('Unsuitable', 'Suitable', 'Limitations', 'Mixed'))
@@ -41,8 +44,8 @@ get_zns <- function(prc){
     geom_tile(data = tbl, aes(x = x, y = y, fill = class)) + 
     scale_fill_manual(values = c('white', 'darkgreen', 'darkgrey', '#ffffc6')) +
     geom_sf(data = wrld, fill = NA) + 
-    ggtitle(label = glue('Percentile: {prc}% / Thr prob: {round(thr.prb, 2)} / Thr uncr: {round(thr.unc, 2)}')) +
-    coord_sf() + 
+    ggtitle(label = glue('Percentile: {prc.prb}%-{prc.unc}% / Thr prob: {round(thr.prb, 2)} / Thr uncr: {round(thr.unc, 2)}')) +
+    coord_sf(xlim = extent(brs)[1:2], ylim = extent(brs)[3:4]) + 
     theme_void() + 
     theme(legend.position = 'bottom', 
           plot.title = element_text(size = 14, hjust = 0.5)) +
@@ -56,7 +59,10 @@ get_zns <- function(prc){
 
 # Load data ---------------------------------------------------------------
 pnts <- read_csv(glue('../data/tbl/run_4/03 points RF wcl 2 0.csv'))[,1:2]
+# wrld <- st_read('../data/shp/base/all_countries.shp') %>% filter(CONTINENT == 'Africa')
+wrld <- st_read('../data/shp/base/countries_target_4.shp')
 
+# Clustering
 fles <- dir_ls('../rf/output/run_4/wc 20/results/raw/future', regexp = '.tif$')
 fles <- grep('clust', fles, value = T)
 fles <- as.character(fles)
@@ -64,6 +70,14 @@ fles <- as.character(fles)
 # To calculate the modal raster -------------------------------------------
 stck <- raster::stack(fles)
 mdal <- raster::modal(stck)
+
+# Probability -------------------------------------------------------------
+fles <- dir_ls('../rf/output/run_4/wc 20/results/raw/future', regexp = '.tif$')
+fles <- grep('rob', fles, value = T)
+fles <- as.character(fles)
+
+prob <- raster::stack(fles)
+prob <- raster::mean(prob)
 
 # To calculate the average uncertainty ------------------------------------
 fles <- dir_ls('../rf/output/run_4/wc 20/results/raw/future')
@@ -87,8 +101,12 @@ qntl <- as_tibble(qntl)
 qntl <- mutate(qntl, percentage = parse_number(percentage))
 
 # Create rasters and map --------------------------------------------------
-plts <- map(.x = 1:12, .f = get_zns)
-gall <- ggarrange(plotlist = plts, ncol = 3, nrow = 4)
-ggsave(plot = gall, filename = '../png/maps/thr/limitations_mixed_future_modal_2_0.png', units = 'in', width = 16, height = 15, dpi = 300)
+
+# Combinations 
+cmbn <- expand.grid(seq(5, 25, 5), c(5, 25, 5))
+plts <- map2(.x = c(5, 10, 15, 20, 25, 10), .y = c(15, 15, 15, 15, 20, 20), .f = get_zns) # plts <- map(.x = 1:12, .f = get_zns)
+plts <- map2(.x = cmbn$Var1, .y = cmbn$Var2, .f = get_zns)
+gall <- ggarrange(plotlist = plts, ncol = 4, nrow = 4)
+ggsave(plot = gall, filename = '../png/maps/thr/limitations_mixed_future_modal_2_0_v3.jpg', units = 'in', width = 20, height = 20, dpi = 300)
 
 
